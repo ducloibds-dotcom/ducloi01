f6d520819e0210027c9baf4e8101af33af726406
ducloibds-dotcom <ducloibds@gmail.com>
2026-08-12 22:54:04 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE="dangtin";
+var CACHE="dangtin-codexv49";
 var ASSETS=[self.location.href];
 self.addEventListener("install",function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));
